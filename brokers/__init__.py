bcfa8306eab24d7d515356cf2cc40db2c2edd9c7
Jason Garcia <garciajasonmayra2003@gmail.com>
2025-10-01 22:52:20 -0700

    Add in-memory paper broker implementation

--- brokers/__init__.py
+++ brokers/__init__.py
@@ -1,4 +1,6 @@
 """Broker implementations and abstractions."""
 from .broker_base import Broker
 from .alpaca_broker import AlpacaBroker
-__all__ = ["Broker", "AlpacaBroker"]
+from .paper_broker import PaperBroker
+
+__all__ = ["Broker", "AlpacaBroker", "PaperBroker"]
