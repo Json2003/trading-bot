23795c934928b6ec04be00d6e6c9305b07f79bc6
Jason Garcia <garciajasonmayra2003@gmail.com>
2025-10-01 23:32:12 -0700

    Make reconciler compatible with legacy broker API

--- brokers/__init__.py
+++ brokers/__init__.py
@@ -1,4 +1,6 @@
 """Broker implementations and abstractions."""
 from .broker_base import Broker
 from .alpaca_broker import AlpacaBroker
-__all__ = ["Broker", "AlpacaBroker"]
+from .reconciler import Reconciler, RiskLimits
+
+__all__ = ["Broker", "AlpacaBroker", "Reconciler", "RiskLimits"]
