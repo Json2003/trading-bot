9ac387333efb2deaed0262e6b1cb57deedae2e82
Jason Garcia <garciajasonmayra2003@gmail.com>
2025-10-01 22:24:54 -0700

    Add helper to persist result artifacts with metadata

--- tradingbot_core/__init__.py
+++ tradingbot_core/__init__.py
@@ -3,6 +3,7 @@
 from .config import ConfigBundle, load_config
 from .logging_setup import setup_logging
 from .backtest_save import save_backtest_results, load_backtest_results
+from .results import deps_fingerprint, save_results
 
 __all__ = [
     "ConfigBundle",
@@ -10,4 +11,6 @@ __all__ = [
     "setup_logging",
     "save_backtest_results",
     "load_backtest_results",
+    "deps_fingerprint",
+    "save_results",
 ]
