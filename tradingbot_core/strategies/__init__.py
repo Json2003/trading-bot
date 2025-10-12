e6d6d063f27bbc5e3248a263745c6a45f220a7ff
Jason Garcia <garciajasonmayra2003@gmail.com>
2025-10-12 09:46:31 -0700

    Add CCXT multi-strategy backtest example

--- tradingbot_core/strategies/__init__.py
+++ tradingbot_core/strategies/__init__.py
@@ -1,6 +1,14 @@
 """Strategy implementations built on the lightweight core protocol."""
 
+from .arbitrage import CrossExArb
 from .dca import DCAMartingale
 from .grid import GridConfig, GridStrategy
+from .momentum import MomentumEMA
 
-__all__ = ["DCAMartingale", "GridConfig", "GridStrategy"]
+__all__ = [
+    "CrossExArb",
+    "DCAMartingale",
+    "GridConfig",
+    "GridStrategy",
+    "MomentumEMA",
+]
