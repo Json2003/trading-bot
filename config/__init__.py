d4cd2516af5347db82d50c5e45c8638bcba22540
Jason Garcia <garciajasonmayra2003@gmail.com>
2025-10-23 01:43:27 +0000

    new

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+"""Configuration package for portfolio and environment settings.
+
+This file marks the directory as a Python package to avoid ambiguities in
+type-checkers and module resolution.
+"""
+
+__all__ = [
+    "portfolio_loader",
+]
